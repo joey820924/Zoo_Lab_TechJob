253d50dc75aefe1df54b2ef1cdaf3088256d5190
ChiaLun <engineerguitar@gmail.com>
2017-01-19 19:36:26 +0800

    add crawler

--- test_crawler.py
+++ test_crawler.py
@@ -1,5 +1,16 @@
+''''
 import requests
 re=requests.get('https://www.ptt.cc/bbs/Tech_Job/index.html')
 f = open('news.txt','a')
 f.write(re.text.encode('utf-8')) 
 print re.text.encode('utf-8')
+'''
+
+import requests
+from bs4 import BeautifulSoup
+re=requests.get('https://www.ptt.cc/bbs/Tech_Job/index.html')
+#  print re.text.encode('utf-8')
+soup=BeautifulSoup(re.text.encode('utf-8'), "html.parser")
+for line in soup.select('.r-ent'):
+    print line.select('.title')[0].text.encode('utf-8')
+    print line.select('.date')[0].text.encode('utf-8'),line.select('.author')[0].text.encode('utf-8')
