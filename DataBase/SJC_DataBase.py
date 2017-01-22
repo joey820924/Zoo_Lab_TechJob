196c776407cfe31091469cb51ae7a7cd50fa0393
joey <joey@Joeyde-MacBook-Pro.local>
2017-01-22 22:08:15 +0800

    Download database to Json
    Download data from database and make a plot for analyze

--- DataBase/SJC_DataBase.py
+++ DataBase/SJC_DataBase.py
@@ -1,18 +1,19 @@
-''' coding=utf-8'''
+# -*- coding: utf-8 -*-
 import MySQLdb
 import json
 import sys
 import os
 reload(sys)
-sys.setdefaultencoding('utf-8')
+exec("sys.setdefaultencoding('utf-8')")  
+assert sys.getdefaultencoding().lower() == "utf-8" 
 def GetTableName():
     conn = MySQLdb.connect(host='127.0.0.1',user='root',passwd='joey820924',db='Soft_Job_Comment',charset='utf8')
     cursor = conn.cursor()
-    sql = 'SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = "Soft_Job_Comment"'
+    sql = 'SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = "Soft_Job_Comment"'  #取得相對應的ID以取得Soft_Job_Comment內的資料
     cursor.execute(sql)
     data = cursor.fetchall()
     cursor.close()
-    with open('TABLE_NAME.json','w+') as f:
+    with open('TABLE_NAME.json','w+') as f: #w+是打開後清空裡面資料，可讀可寫，若檔案不存在則建立
         for row in data:
             result = {}
             result['TABLE_NAME'] = str(row[0])
@@ -29,7 +30,7 @@ def createJson():
     if os.path.exists('SJC.json'):
         os.remove('SJC.json')
     file = open(path,'r')
-    for line in file.readlines():
+    for line in file.readlines():  #因json讀取一次只能讀取一組，所以當有很多時就必須要分行讀取，不然就是要在最開始建立json檔時就要把它包成一個list
         dic = json.loads(line)
         TableName = str(dic['TABLE_NAME'])
         sql = 'SELECT * FROM '+TableName
@@ -41,7 +42,7 @@ def createJson():
             column_list.append(i[0])
         
 
-        with open('SJC.json','a') as f:
+        with open('SJC.json','a') as f:  #a 是 append的意思
             for row in data:
                 result = {}
                 result['User'] = str(row[0])
